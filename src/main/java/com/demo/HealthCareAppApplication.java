7e9be70a193f5c9c9944c00301a2320f9d06646c
saki-tanaka-uhuru <tanaka.saki@uhuru.jp>
2022-06-23 02:51:45 +0900

    update for deploy

--- src/main/java/com/demo/HealthCareAppApplication.java
+++ src/main/java/com/demo/HealthCareAppApplication.java
@@ -2,9 +2,16 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HealthCareAppApplication {
+public class HealthCareAppApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HealthCareAppApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HealthCareAppApplication.class, args);
